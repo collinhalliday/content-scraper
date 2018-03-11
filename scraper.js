fe39869f908430fd7f1ef306f765e75403f8291e
Collin Halliday <collinhalliday@gmail.com>
2018-03-10 18:54:53 -0700

    Created a csv file creator with npm plugin that creates csv file with data retrieved from content scraper if a file of same name does not already exist.

--- scraper.js
+++ scraper.js
@@ -1,12 +1,14 @@
+const createCsvWriter = require('csv-writer').createArrayCsvWriter;
 const fs = require('fs');
-var osmosis = require("osmosis");
-var url = require("url");
+const osmosis = require("osmosis");
 
 const directory = 'data';
 const prices = [];
 const descriptions = [];
 const imageUrls = [];
 const pageUrls = [];
+const csvData = [];
+let tempArray = [];
 
 //Checks whether or not 'data' directory exists. If not, said directory is created.
 if(!fs.existsSync(directory)) {
@@ -18,39 +20,61 @@ if(!fs.existsSync(directory)) {
 osmosis
 	.get('http://shirts4mike.com/')
 	.find('li.shirts > a')
-	// .set('location')
 	.follow('@href')
 	.find('div.shirts > div.wrapper > ul > li > a')
-	.set('location')
 	.follow('@href')
 	.set({
 	    'price':        '.price',
 	    'description':  'div.shirt-details > h1',
 			'imageUrl':       'img@src'
 	})
+	.data(function(tShirtInfo) {
+		 tempArray = [];
+		 tempArray.push(tShirtInfo.price);
+		 tempArray.push(tShirtInfo.description.replace(/\$\d+ /, ''));
+		 tempArray.push(tShirtInfo.imageUrl);
+	})
 	.then(function(window) {
 		let url = window.location.href;
-		pageUrls.push(url);
+		tempArray.push(url);
+		tempArray.push(getTime());
+		csvData.push(tempArray);
 	})
-	.data(function(tShirtInfo) {
-		 prices.push(tShirtInfo.price);
-		 descriptions.push(tShirtInfo.description);
-		 imageUrls.push(tShirtInfo.imageUrl);
+	.done(function() {
+		if(!fs.existsSync(directory + '/' + getDate() + '.csv'))
+			createCSV();
+		else {
+			fs.unlinkSync(directory + '/' + getDate() + '.csv');
+			createCSV();
+		}
 	})
 	// .log(console.log)
 	// .error(console.log)
 	// .debug(console.log)
 
-setTimeout(printInfo, 5000);
-
-function printInfo() {
-	let tShirt = 1;
-	prices.forEach(function(price) {
-		console.log("TShirt " + tShirt);
-		console.log("Price: " + price);
-		console.log("Description: " + descriptions[tShirt - 1]);
-		console.log("PageURL: " + pageUrls[tShirt - 1]);
-		console.log("ImageURL: " + imageUrls[tShirt - 1] + "\n");
-		tShirt++;
+//Creating CSV file through use of csv-writer package and csvData array.
+function createCSV() {
+	const csvWriter = createCsvWriter({
+	    header: ['PRICE', 'TITLE', 'IMAGE URL', 'URL', 'TIME'],
+	    path: directory + '/' + getDate() + '.csv'
 	});
+
+	csvWriter.writeRecords(csvData)       // returns a promise
+	    .then(() => {
+	        console.log('...CSV File Written');
+	    });
+}
+
+function getDate() {
+	let date = new Date();
+	let year = date.getFullYear();
+	let month = date.getMonth();
+	let day = date.getDate();
+	return year + '-' + month + '-' + day;
+}
+
+function getTime() {
+	let date = new Date();
+	let dateArray = date.toString().split(' ');
+	return dateArray[4];
 }
